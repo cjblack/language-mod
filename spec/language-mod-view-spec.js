b9649718202613a8580959d736fb73746b5c6440
C.J.  Black <christopher_black@brown.edu>
2017-01-23 12:15:24 -0500

    quick code for mod file syntax

---
+++ spec/language-mod-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageModView from '../lib/language-mod-view';
+
+describe('LanguageModView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
